5b4ff0173d8cb396791c4887d21e3c8be7c2c7bc
neeschal00 <nischal06khatri@gmail.com>
2023-10-04 19:09:11 +0545

    setup the project according to given template

--- api.py
+++ api.py
@@ -1,1 +1,19 @@
-#API code here 
+import requests
+
+def get_url(url: str):
+    """
+    Function that will call a provide GET API endpoint url and return its status code and either its content or error message as a string
+
+    Parameters
+    ----------
+    url : str
+        URL of the GET API endpoint to be called
+
+    Returns
+    -------
+    int
+        API call response status code
+    str
+        Text from API call response
+    """
+
--- currency.py
+++ currency.py
@@ -1,1 +1,53 @@
-#Currency converter code here" 
+
+def round_rate(rate):
+    """
+    Function that will round an input float to 4 decimals places.
+
+    Parameters
+    ----------
+    rate: float
+        Rate to be rounded
+
+    Returns
+    -------
+    float
+        Rounded rate
+    """
+    
+
+def reverse_rate(rate):
+    """
+    Function that will calculate the inverse rate from the provided input rate.
+    It will check if the provided input rate is not equal to zero.
+    If it not the case, it will calculate the inverse rate and round it to 4 decimal places.
+    Otherwise it will return zero.
+
+    Parameters
+    ----------
+    rate: float
+        FX conversion rate to be inverted
+
+    Returns
+    -------
+    float
+        Inverse of input FX conversion rate
+    """
+    
+def format_output(date, from_currency, to_currency, rate, amount):
+    """
+    Function that will calculate the inverse rate from the provided input rate.
+    It will check if the provided input rate is not equal to zero.
+    If it not the case, it will calculate the inverse rate and round it to 4 decimal places.
+    Otherwise it will return zero.
+
+    Parameters
+    ----------
+    rate: float
+        FX conversion rate to be inverted
+
+    Returns
+    -------
+    float
+        Inverse of input FX conversion rate
+    """
+   
